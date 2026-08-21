944cf65404659ef1fa91ed9ddf7b564526848a70
Volodymyr Voloshyn <mailvoloshyn@gmail.com>
2026-08-21 11:12:06 +0300

    Added reporters to project

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   allowCypressEnv: false,
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports/mochawesome',
+    overwrite: false,
+    html: false,
+    json: true
+  },
   e2e: {
     baseUrl: 'https://practicesoftwaretesting.com',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    }
+    setupNodeEvents(on, config) {}
   }
 })
