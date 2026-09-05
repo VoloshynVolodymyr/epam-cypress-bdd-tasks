51d8c6812c4e7b28c5fcd6f7241efb96065cd257
Volodymyr Voloshyn <mailvoloshyn@gmail.com>
2026-09-05 18:35:26 +0300

    Try overriding userAgent to bypass Cloudflare bot detection

--- cypress.config.ts
+++ cypress.config.ts
@@ -11,6 +11,8 @@ export default defineConfig({
   },
   e2e: {
     baseUrl: 'https://practicesoftwaretesting.com',
+    userAgent:
+      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36',
     setupNodeEvents(on) {
       on('task', {
         log(message) {
